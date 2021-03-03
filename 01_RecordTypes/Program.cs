b661c8295512d79d500884070cad8dbcd44142bd
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2021-03-03 18:11:09 -0300

    Transformando a classe Endereco em Record
    Ganhamos de brinde do Record:
    - Construtor
    - Método Equals por valores
    - Copia de objetos usando with expression
    - Método ToString que exibe propriedade/valor

--- 01_RecordTypes/Program.cs
+++ 01_RecordTypes/Program.cs
@@ -15,41 +15,10 @@ namespace _01_RecordTypes
             var enderecosIguais = enderecoAna.Equals(enderecoCris);
             Console.WriteLine("Os endereços são {0}", enderecosIguais ? "iguais" : "diferentes");
 
-            var enderecoAmigaDaCris = enderecoCris.ComNumero(200);
+            var enderecoAmigaDaCris = enderecoCris with { Numero = 200 };
             Console.WriteLine(enderecoAmigaDaCris);
         }
     }
 
-    public class Endereco : IEquatable<Endereco>
-    {
-        public Endereco(string cep, int numero, string cidade)
-        {
-            this.Cep = cep;
-            this.Numero = numero;
-            this.Cidade = cidade;
-        }
-
-        public string Cep { get; set; }
-        public int Numero { get; set; }
-        public string Cidade { get; set; }
-
-        public Endereco ComNumero(int numero) => new Endereco(this.Cep, numero, this.Cidade);
-
-        public bool Equals(Endereco other)
-        {
-            if (other is null)
-            {
-                return false;
-            }
-
-            return this.Cep == other.Cep
-                && this.Numero == other.Numero
-                && this.Cidade == other.Cidade;
-        }
-
-        public override string ToString()
-        {
-            return $"Cep: {this.Cep}, Numero: {this.Numero}, Cidade: {this.Cidade}";
-        }
-    }
+    public record Endereco(string Cep, int Numero, string Cidade);
 }
